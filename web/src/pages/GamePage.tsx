f7157d2c7ca70447b795a8fd879184e22066eeb6
Tim <herew26@gmail.com>
2024-12-23 12:56:01 +0300

    ✨ Wrap pages into `Page` component

--- web/src/pages/GamePage.tsx
+++ web/src/pages/GamePage.tsx
@@ -4,25 +4,28 @@ import Board from '@/components/Game/components/board';
 import "@/components/Game/styles/globals.css";
 import { List } from '@telegram-apps/telegram-ui';
 import { SectionHeader } from '@telegram-apps/telegram-ui/dist/components/Blocks/Section/components/SectionHeader/SectionHeader';
+import { Page } from '@/components/Page';
 
 const GamePage: React.FC = () => {
   return (
-    <List>
-      <SectionHeader large>
-        2048 от Магнит Маркета
-      </SectionHeader>
-      <Score />
+    <Page back={false}>
+      <List>
+        <SectionHeader large>
+          2048 от Магнит Маркета
+        </SectionHeader>
+        <Score />
 
-      <div
-        style={{
-          marginTop: '1em',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
-        <Board />
-      </div>
-    </List>
+        <div
+          style={{
+            marginTop: '1em',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+          <Board />
+        </div>
+      </List>
+    </Page>
     // </div>
 
     // <List>
--- web/src/pages/PrizesPage.tsx
+++ web/src/pages/PrizesPage.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
+import { Page } from '@/components/Page';
 
 const PrizesPage: React.FC = () => {
   return (
-    <Box sx={{ padding: 2 }}>
-      <Typography variant="h5" component="h1" gutterBottom>
-        Призы
-      </Typography>
-      {/* Add your prizes content here */}
-    </Box>
+    <Page>
+      <Box sx={{ padding: 2 }}>
+        <Typography variant="h5" component="h1" gutterBottom>
+          Призы
+        </Typography>
+        {/* Add your prizes content here */}
+      </Box>
+    </Page>
   );
 };
 
