c1dca8e3de6a1f36841c109dcad2fab1ed50cab1
Tim <herew26@gmail.com>
2024-12-26 19:48:35 +0300

    👽️ Update web api client

---
+++ web/src/api/promocodeTypesApi.tsx
@@ -0,0 +1,11 @@
+import { PromocodeType } from '@/models/promocodeType';
+import apiClient from './apiConfig';
+
+const promocodeTypesApi = apiClient;
+
+export const promocodeTypesUrlEndpoint = "/promocode-types";
+
+export const getAllPromocodeTypes = async (): Promise<PromocodeType[]> => {
+    const response = await promocodeTypesApi.get<PromocodeType[]>(promocodeTypesUrlEndpoint);
+    return response.data;
+}
