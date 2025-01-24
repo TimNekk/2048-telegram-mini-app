be3da61eaa10389f4ed25f99e7ce2c0d86a4de88
Tim <herew26@gmail.com>
2025-01-24 14:50:41 +0300

    🐛 Add missing `px`

--- web/src/components/Page.tsx
+++ web/src/components/Page.tsx
@@ -106,7 +106,7 @@ export function Page({
             </header>
             <main
                 style={{
-                    paddingBottom: "calc(82px + var(--tg-viewport-safe-area-inset-bottom, 0))",
+                    paddingBottom: "calc(82px + var(--tg-viewport-safe-area-inset-bottom, 0px))",
                     height: "100%",
                     minHeight: "fit-content",
                 }}
